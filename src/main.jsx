2eb4537f8997016abdf2d6522051d6d371e18f17
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-04 19:00:07 +0000

    feat: imports for global styles in app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./styles/reset.css";
 import "./styles/theme.css";
+import "./styles/defaults.css";
+import "./styles/fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
